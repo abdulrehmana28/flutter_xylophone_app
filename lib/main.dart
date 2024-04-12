035daf908b35179419760943ecc36da28ed4f61f
Abdulrehman Azam <abdulrehmana28@proton.me>
2024-04-12 08:52:41 +0500

    Refactored code
    make changes to code for better reliability

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,47 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildButton({required int soundNumber, required Color colorName}) {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        color: colorName,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.black,
         appBar: AppBar(
           leading: const Icon(Icons.play_arrow_outlined),
           title: const Text('Xylophone'),
-          backgroundColor: Colors.black12,
+          backgroundColor: Colors.grey,
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            MaterialButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              color: Colors.red,
-            ),
-            MaterialButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note2.wav'));
-              },
-              color: Colors.orange,
-            ),
-            MaterialButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note3.wav'));
-              },
-              color: Colors.yellow,
-            ),
-            MaterialButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note4.wav'));
-              },
-              color: Colors.green,
-            ),
-            MaterialButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note5.wav'));
-              },
-              color: Colors.teal,
-            ),
-            MaterialButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note6.wav'));
-              },
-              color: Colors.blueGrey,
-            ),
-            MaterialButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note7.wav'));
-              },
-              color: Colors.purple,
-            ),
+            buildButton(soundNumber: 1, colorName: Colors.red),
+            buildButton(soundNumber: 2, colorName: Colors.orange),
+            buildButton(soundNumber: 3, colorName: Colors.yellow),
+            buildButton(soundNumber: 4, colorName: Colors.green),
+            buildButton(soundNumber: 5, colorName: Colors.teal),
+            buildButton(soundNumber: 6, colorName: Colors.blueGrey),
+            buildButton(soundNumber: 7, colorName: Colors.purple),
           ],
         ),
       ),
